8bd9781ac0eaf25082a2a55be767fd5a2ab5dc2e
Jack Pattison <jepatt16@stlawu.edu>
2020-02-03 13:22:32 -0500

    funtion syntax

---
+++ javascript/functions.js
@@ -0,0 +1,13 @@
+// function syntax
+
+// defining a fucntion
+const avg = function(a, b) {
+	return (a + b)/2;
+};
+
+console.log(avg(5, 12));
+
+const print = functions(a, b) {
+	console.log(`a=${a}, b=${b}`);
+};
+
