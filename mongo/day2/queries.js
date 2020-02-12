4c149bc5b27dab7fc8bd5b24d15689a1eee2ff69
Jack Pattison <jepatt16@stlawu.edu>
2020-02-12 13:29:26 -0500

    typical queries

--- mongo/day2/queries.js
+++ mongo/day2/queries.js
@@ -25,3 +25,39 @@ const queries = [
 	// what's everyone's rank
 	Professor.distinct('rank')
 ];
+
+queries[0].exec(function(error, professors) {
+	if (error) console.error(error.stack);
+	const names = professors.map(p => p.name);
+	console.log('Names in order: ', names);
+});
+// Professor.find().sort('-started').limit(1)
+queries[1].exec(function(error, professors) {
+  if (error) console.error(error.stack);
+
+  const names = professors.map(p => p.name);
+  console.log('Started most recently: ', names);
+});
+
+// Professor.find().where('started').equals(2003)
+queries[2].exec(function(error, professors) {
+  if (error) console.error(error.stack);
+
+  const names = professors.map(p => p.name);
+  console.log('Started in 2003: ', names);
+});
+
+// Professor.find().where('courses').in(362)
+queries[3].exec(function(error, professors) {
+  if (error) console.error(error.stack);
+
+  const names = professors.map(p => p.name);
+  console.log('Teaches 362: ', names);
+});
+
+// Professor.distinct('rank')
+queries[4].exec(function(error, ranks) {
+  if (error) console.error(error.stack);
+
+  console.log('Distinct ranks: ', ranks);
+});
