02a4206c0e987332c470d1364730f4aa486f66eb
jepatt16 <jepatt16@gmail.com>
2020-03-04 13:39:11 -0500

    more advanced requests

--- express/hello/app.js
+++ express/hello/app.js
@@ -4,11 +4,34 @@ const express = require('express');
 // create server
 const app = express();
 
-// respond to a request
+// respond to requests
+
+// cs-linuxlab-31.stlawu.edu:3000/
 // 'get' here is the http method
 app.get('/', function(req, resp){
   resp.send('jello world');
 });
+
+// cs-linuxlab-31.stlawu.edu:3000/foo
+app.get('/foo', function(req, resp){
+  resp.send('hello foo');
+});
+
+// cs-linuxlab-31.stlawu.edu:3000/foo/bar
+app.get('/foo/bar', function(req, resp){
+  resp.send('hello foobar');
+});
+
+// cs-linuxlab-31.stlawu.edu:3000/zap/z (for any z)
+app.get('/zap/:z', function(req, resp){
+  resp.send(`hello ${req.params.z}`);
+});
+
+// cs-linuxlab-31.stlawu.edu:3000/zap?x=a&y=b
+app.get('/zap', function(req, resp){
+  resp.send(`hello ${req.query.x} and ${req.query.y}`);
+});
+
 
 // start the server on port 3000
 // traditionally you use port 80, but you need admin privileges
