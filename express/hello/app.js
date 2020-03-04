9435a7a58ee2e0ae75efa5a094eb3be4cfbb19d1
jepatt16 <jepatt16@gmail.com>
2020-03-04 13:05:34 -0500

    hello world web server

---
+++ express/hello/app.js
@@ -0,0 +1,18 @@
+// hello world web server
+const express = require('express');
+
+// create server
+const app = express();
+
+// respond to a request
+// 'get' here is the http method
+app.get('/', function(req, resp){
+  response.send('jello world');
+});
+
+// start the server on port 3000
+// traditionally you use port 80, but you need admin privileges
+// also since it's in 'development' we should use a lower
+// priority (higher value) port number
+app.listen(3000);
+console.log('the server will see you now');
